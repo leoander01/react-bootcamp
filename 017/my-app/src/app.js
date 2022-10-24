35c8c4a15f11f795111589629e90950c68138892
leoander01 <leoanderneves8@outlook.com>
2022-10-24 17:10:43 -0300

    Common uses for the useEffect hook

---
+++ 017/my-app/src/app.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [counter, setCounter] = useState(1)
+
+  return (
+    <>
+      {counter > 0 && <Counter counter={counter} />}
+      <button onClick={() => setCounter(c => c - 1)}>-</button>
+      <button onClick={() => setCounter(c => c + 1)}>+</button>
+    </>
+  )
+}
+
+function Counter({ counter }) {
+  console.log('counter')
+
+  useEffect(() => {
+    console.log('useEffect de counter!')
+
+    // clean
+    return () => {
+      console.log('clean effect de counter')
+    }
+  })
+
+  return (
+    <>
+      <h1>{counter}</h1>
+    </>
+  )
+}
+
+export default App
